fc9aeee40a1a43c2ffe01340b3a17b76ed8db201
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-09 00:00:24 +0300

    CurrentWeather Component

--- src/component/WeatherInfo.jsx
+++ src/component/WeatherInfo.jsx
@@ -1,2 +1,7 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
+
+function CurrentWeather(){
+  const[weatherData,setWeatherData] = useState(null);
+  const[error,setError] = useState(null);
+}
