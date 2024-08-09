5b2bda114be218763b2e24ba31f2eef3dae80164
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-09 12:27:37 +0300

    imported api key

--- src/component/WeatherInfo.jsx
+++ src/component/WeatherInfo.jsx
@@ -1,7 +1,15 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
 
+const weather_API_Key = import.meta.env.VITE_API_KEY;
+
 function CurrentWeather(){
   const[weatherData,setWeatherData] = useState(null);
   const[error,setError] = useState(null);
+
+
+
+
+
+
 }
