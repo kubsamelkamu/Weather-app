3ebf437c3ca819c7a707b6d46d388ba19e5c4dfa
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-09 12:34:45 +0300

    useeffect hook used to fetch weather info

--- src/component/WeatherInfo.jsx
+++ src/component/WeatherInfo.jsx
@@ -7,8 +7,21 @@ function CurrentWeather(){
   const[weatherData,setWeatherData] = useState(null);
   const[error,setError] = useState(null);
 
+   useEffect(()=>{
+    const fetchWeatherData = async () =>{
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${weather_API_Key}`
+        );
+        setWeatherData(response.data);
+      } catch (error) {
+        setError(error.message);
+        alert('Error getting weather data');
+      }
+    }
 
-
+    fetchWeatherData();
+   },[]);
 
 
 
