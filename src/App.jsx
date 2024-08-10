d33281a4bad18ae90297bf1160658a1c94f59f7f
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 19:16:57 +0300

    created div container

--- src/App.jsx
+++ src/App.jsx
@@ -8,4 +8,10 @@ function MainSection(){
       setWeatherDate(data);
       setCity(data?data.name:'')
   }
+
+  return(
+    <div className="main-section-container flex-grow bg-gray-100 p-4 md:p-10">
+
+    </div>
+  )
 }
