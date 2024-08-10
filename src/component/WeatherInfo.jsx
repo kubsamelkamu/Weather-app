b7ad4839394da6ef088ccaf80da226185becac4b
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 09:55:41 +0300

    make container visually appeling

--- src/component/WeatherInfo.jsx
+++ src/component/WeatherInfo.jsx
@@ -9,23 +9,17 @@ function CurrentWeather ({weatherData }) {
     return <div>Loading...</div>;
   }
 
-  const { name, main, weather, wind } = weatherData;
+  const { name, main, weather,wind } = weatherData;
   const weatherIconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
 
   return (
-    <div className="bg-white shadow-md rounded p-4 max-w-sm mx-auto">
-      <h2 className="text-2xl font-bold mb-2">{name}</h2>
-      <div className="flex items-center mb-4">
-        <img src={weatherIconUrl} alt={weather[0].description} className="w-12 h-12 mr-4" />
-        <div>
-          <p className="text-xl text-gray-700 capitalize">{weather[0].description}</p>
-          <p className="text-3xl text-gray-800">{main.temp}°C</p>
-        </div>
-      </div>
-      <div className="text-gray-600">
-        <p>Humidity: {main.humidity}%</p>
-        <p>Wind Speed: {wind.speed} m/s</p>
-      </div>
+    <div className="bg-blue-500 text-white shadow-md rounded-lg p-8 max-w-3xl w-full mx-auto h-96 flex flex-col items-center justify-center">
+      <h2 className="text-4xl font-bold mb-4">{name}</h2>
+      <img src={weatherIconUrl} alt={weather[0].description} className="w-32 h-32 mb-4" />
+      <p className="text-2xl">{weather[0].description}</p>
+      <p className="text-2xl mb-2">Temperature:{main.temp}°C</p>
+      <p className="text-2xl mb-2">Humidity: {main.humidity}%</p>
+      <p className="text-2xl mb-2">Wind Speed: {wind.speed} m/s</p>
     </div>
   );
 };
@@ -42,31 +36,16 @@ export const  MainSection = () => {
   };
 
   return (
-    <div className="main-section-container">
+    <div className="main-section-container flex-grow bg-gray-100 p-4 md:p-10">
       <CitySearch onWeatherData={handleWeatherData} />
       {weatherData ? (
-        <CurrentWeather2 weatherData={weatherData} />
+        <CurrentWeather weatherData={weatherData} />
       ) : (
         <p className="text-center">Enter a city name to check the weather.</p>
       )}
     </div>
   );
 };
-
-
-
-
-
-export const Main = () => {
-  return (
-    <main className="flex-grow bg-gray-100 p-4 md:p-10">
-      <CitySearch/>
-   
-    </main>
-  );
-};
-
-
 
 
 export default CurrentWeather;
