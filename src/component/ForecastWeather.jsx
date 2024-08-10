ca8a92fd6974bb8799e051a544de451e829d2caf
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 18:44:13 +0300

    imported PropTypes

--- src/component/ForecastWeather.jsx
+++ src/component/ForecastWeather.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import PropTypes from 'prop-types';
 
 function Forecast({city}){
     const[forecastData,setForecastData] = useState(null);
