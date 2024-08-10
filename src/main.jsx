114c6b6a43c313951311ddf7390c741eee903099
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 21:39:27 +0300

    imported Header and MainSection

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import './index.css'
+import Header from './component/Header'
+
+import MainSection from './App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header/>
+    <MainSection/>
   </StrictMode>,
 )
