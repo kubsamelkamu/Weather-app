b235e9028cc98f33ef92319e8caaee5b48d15c20
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 09:03:36 +0300

    fetchweather inside useeffect

--- src/component/City.jsx
+++ src/component/City.jsx
@@ -1,6 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CitySearch({onWeatherData}){
   const[city,setCity] = useState('');
   const[error,setError] = useState(null);
+
+  useEffect(()=>{
+    const fetchWeatherData = async()=>{
+      if (!city) {
+        return;
+      }
+    }
+  })
 }
