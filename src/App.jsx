02f293177fcf39f06a6774181d440421faf563e8
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 19:14:33 +0300

    handle-weatherData method

--- src/App.jsx
+++ src/App.jsx
@@ -3,4 +3,9 @@ import { useState } from "react";
 function MainSection(){
   const[weatherDate,setWeatherDate] = useState(null);
   const[city,setCity] = useState(null);
+
+  const handleWeatherData=(data)=>{
+      setWeatherDate(data);
+      setCity(data?data.name:'')
+  }
 }
