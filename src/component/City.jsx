e1625fd39954ec74aaeb972f23d6ecdbfbafe8c4
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 09:18:31 +0300

    container for user input

--- src/component/City.jsx
+++ src/component/City.jsx
@@ -29,5 +29,26 @@ function CitySearch({onWeatherData}){
       }
     }
     fetchWeatherData();
-  },[])
+  },[city, onWeatherData])
+
+  return (
+    <div className="bg-white shadow-md rounded p-6 max-w-lg w-full mx-auto mb-4 h-56 flex flex-col justify-between">
+      <img 
+        src="https://openweathermap.org/img/wn/01d@2x.png" 
+        alt="Weather Icon"
+        className="w-16 h-16 mx-auto"
+      />
+      <div className="flex-grow">
+        <h3 className="text-lg font-bold mb-2 text-center">Search for a City</h3>
+        <input
+          type="text"
+          value={city}
+          onChange={}
+          placeholder="Enter city name"
+          className="w-full p-3 border border-gray-300 rounded mb-2"
+        />
+      </div>
+      {error && <p className="text-red-500 mt-2 text-center">{error}</p>}
+    </div>
+  );
 }
