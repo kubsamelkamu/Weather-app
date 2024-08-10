a83f3bd5ef4c57a8a5fc740008446325f7839e7c
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-10 18:46:12 +0300

    imported api_key from .env file

--- src/component/ForecastWeather.jsx
+++ src/component/ForecastWeather.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from 'prop-types';
 
+
+const openweathermap_api_key = import.meta.env.VITE_API_KEY;
 function Forecast({city}){
     const[forecastData,setForecastData] = useState(null);
     const[error,setError] = useState(null);
