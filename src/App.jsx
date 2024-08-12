d95604642013480fdfc2f299a3fc9e1d2e6c6c57
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-12 11:06:56 +0300

    conditionally rendering the Forecast card

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,37 @@ import CitySearch from "./component/City";
 import CurrentWeather from "./component/WeatherInfo";
 import Forecast from "./component/ForecastWeather";
 
-function MainSection(){
-  const[weatherDate,setWeatherDate] = useState(null);
-  const[city,setCity] = useState(null);
-  const[showForecast,setShowForecast] = useState(false);
+function MainSection() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState(null);
+  const [showForecast, setShowForecast] = useState(false);
 
-  const handleWeatherData=(data)=>{
-      setWeatherDate(data);
-      setCity(data?data.name:'')
-      setShowForecast(false);
-  }
+  const handleWeatherData = (data) => {
+    setWeatherData(data);
+    setCity(data ? data.name : '');
+  };
 
-  const toggleForecast=()=>{
+  const toggleForecast = () => {
     setShowForecast(!showForecast);
   };
 
-  return(
+  return (
     <div className="main-section-container flex-grow bg-gray-100 p-4 md:p-10">
       <CitySearch onWeatherData={handleWeatherData} />
-      {weatherDate?(
+      {weatherData ? (
         <>
-          <CurrentWeather weatherData={weatherDate} />
-          <div className="text-ceter mt-4">
-              <button
-                onClick={toggleForecast}
-                className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-              >
-                {showForecast?'Hide 5 Day forecast':'Show 5 Day forecast'}
-              </button>
+          <CurrentWeather weatherData={weatherData} />
+          <div className="text-center mt-4">
+            <button
+              onClick={toggleForecast}
+              className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+            >
+              {showForecast ? 'Hide 5 Day Forecast' : 'Show 5 Day Forecast'}
+            </button>
           </div>
-          <Forecast city={city} />
+          {showForecast && <Forecast city={city} />}
         </>
-      ):(
+      ) : (
         <p className="text-center">Enter a city name to check the weather.</p>
       )}
     </div>
