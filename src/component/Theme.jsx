91e6b9b441b5cba1e0270c928f7a0bbb51041d36
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-13 07:44:26 +0300

    use Effect hook to render theme

--- src/component/Theme.jsx
+++ src/component/Theme.jsx
@@ -1,5 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ThemeSwitcher{
     const[theme,setTheme] = useState('light');
+
+    useEffect(()=>{
+        const savesTheme = localStorage.getItem('theme')||'light';
+        setTheme(savesTheme);
+        document.documentElement.setAttribute('data-theme',savesTheme);
+    },[])
 }
