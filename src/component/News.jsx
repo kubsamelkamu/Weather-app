63796ebc045023c557070d0193baf32e2f18f785
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-13 10:05:34 +0300

    useEffect hook

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
 import axios from "axios";
+import { useEffect } from "react";
 
 const news_api_key = import.meta.env.VITE_NEWS_API_KEY;
 function WeatherNews({city}){
     const[article,setArticle] = useState([]);
     const[error,setError]=useState(null);
+
+    useEffect(()=>{
+        const fetchNews = async()=>{
+            if (!city) {
+                setArticle([]);                
+            }
+        }
+    })
+
+
 }
 
 WeatherNews.propTypes ={
