7cb97f197dc4a2ee5993e15b612fcaddfeebbdc3
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-13 09:58:41 +0300

    newsapi from env file

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -1,5 +1,7 @@
 const { useState } = require("react");
 
+
+const news_api_key = import.meta.env.VITE_NEWS_API_KEY;
 function WeatherNews(){
     const[article,setArticle] = useState([]);
     const[error,setError]=useState(null);
