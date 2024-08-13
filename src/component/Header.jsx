cbdfd491d7b9c8e89f32ae855aaf08b0b582d455
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-13 07:59:23 +0300

    call Theme switcher

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-blue-500 text-white p-4 text-center md:text-left flex justify-between items-center">
       <h1 className="text-2xl md:text-3xl">Weather Application</h1>
-  
+      <ThemeSwitcher/>
     </header>
   );
 };
