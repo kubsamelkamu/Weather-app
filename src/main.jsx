9c40652f2312490dceee93017554db5554f84e3c
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-14 11:19:46 +0300

    imported App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
-import Header from './component/Header'
 
-import MainSection from './App'
+import App from './App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
-    <MainSection/>
+   <App/>
   </StrictMode>,
 )
